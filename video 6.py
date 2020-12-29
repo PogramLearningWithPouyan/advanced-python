7ccf98cfc297353843400f75a666e06ae0e0460e
PogramLearningWithPouyan <70820602+PogramLearningWithPouyan@users.noreply.github.com>
2020-12-29 15:25:28 +0330

    Add files via upload

---
+++ video 6.py
@@ -0,0 +1,11 @@
+# num=[1,2,3,4,5,6]
+# num.reverse()
+# reversdname=''
+# print(reversdname.join(list(reversed('hello'))))
+# print(num)
+# x=5
+#
+# print(abs(x))
+# print(sum(num,10))
+y=4.56385247
+print(round(y,4))
