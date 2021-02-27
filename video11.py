1f555bdf17da66d5aa2399cbd4f6c12b8b4b1e2b
PogramLearningWithPouyan <70820602+PogramLearningWithPouyan@users.noreply.github.com>
2021-02-28 00:32:10 +0330

    Add files via upload

---
+++ video11.py
@@ -0,0 +1,6 @@
+import termcolor2
+print(termcolor2.colored('program learning with pouyan',color='red',on_color='on_magenta'))
+print(termcolor2.colored('program learning with pouyan',color='blue'))
+print(termcolor2.colored('program learning with pouyan',color='green'))
+print(termcolor2.colored('program learning with pouyan',color='cyan'))
+help(termcolor2)
