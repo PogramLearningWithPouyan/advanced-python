f24da9a2ba418a242ec1b776db65488e996fc411
PogramLearningWithPouyan <70820602+PogramLearningWithPouyan@users.noreply.github.com>
2021-06-30 17:03:10 +0430

    Add files via upload

---
+++ video27.py
@@ -0,0 +1,86 @@
+# class IUserService:
+#     def GetAllUsers(self):
+#         raise NotImplementedError
+#     def GetUsersById(self): raise NotImplementedError
+#     def CreatNewUser(self): raise NotImplementedError
+# class UserServicesBySql(IUserService):
+#     def CreatNewUser(self):
+#         print('new user is creat')
+#     def GetAllUsers(self):
+#         print('all user is ready')
+#     def GetUsersById(self):
+#         print('all users have an id')
+# class UserServicesByOracle(IUserService):
+#     def CreatNewUser(self):
+#         print('new user is creat')
+#     def GetAllUsers(self):
+#         print('all user is ready')
+#     def GetUsersById(self):
+#         print('all users have an id')
+#
+# user1=UserServicesBySql()
+# user1.GetAllUsers()
+# user2=UserServicesByOracle()
+# user2.GetUsersById()
+
+
+print(5+4)
+print('5'+'4')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class car:
+#     def move(self):
+#         raise NotImplementedError
+#
+#
+# class Benz(car):
+#     def __init__(self,model):
+#         self.model=model
+#     def move(self):
+#         print(f'{self.model} is moving')
+#
+#
+# class BMW(car):
+#     def __init__(self, model):
+#         self.model = model
+#     def move(self):
+#         print(f'{self.model} is moving')
+#
+# class pride(car):
+#     def __init__(self,model):
+#         self.model=model
+#     def move(self):
+#         print(f'{self.model} is moving')
+#
+# cls=Benz('cls')
+# x4=BMW('X4')
+# pride111=pride('111')
+#
+# cls.move()
+# x4.move()
+# pride111.move()
+
+
+
+# numbers=[1,2,3,4,5,6]
+# myNum={1,2,3,4,5,6}
+# mydictunary={'name':'pouyan','family':'rajian'}
+#
+# numbers.copy()
+# myNum.copy()
+# mydictunary.copy()
+# len(numbers)
+# len(myNum)
+
