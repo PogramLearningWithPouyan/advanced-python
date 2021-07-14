7fcc8149102d4d50b1cc95a9a73db24262c293b6
PogramLearningWithPouyan <70820602+PogramLearningWithPouyan@users.noreply.github.com>
2021-07-14 17:12:39 +0430

    Add files via upload

---
+++ video29.py
@@ -0,0 +1,21 @@
+# iterable => list tuple set string
+# iterator
+# iterate
+numlist=[1,2,3,4,5]
+# for num in numlist:
+#     print(num)
+# iternum=iter(numlist)
+# print(next(iternum))
+# print(next(iternum))
+# print(next(iternum))
+# print(next(iternum))
+# print(next(iternum))
+list
+name='pouyan'
+itername=iter(name)
+print(next(itername))
+print(next(itername))
+print(next(itername))
+print(next(itername))
+print(next(itername))
+print(next(itername))
